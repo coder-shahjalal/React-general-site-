503d46edcd11f4c3b9dd4eaa0679c0529890dd15
md shahjalal <shahjalal.freelancer@gmail.com>
2024-01-01 15:16:55 +0600

    start hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Banner from "./components/Banner";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,17 @@
+function Banner() {
+  return (
+    <div className="bg-[#2699fb] w-full py-[50px] ">
+      <div className="max-w-[1240px] mx-auto text-center font-bold">
+        <div className="text-xl md:text-3xl md:p-[24px]">Learn with us</div>
+        <h2 className="text-white font-bold text-5xl md:text-[80px] md:p-[24px]">
+          Grow with us
+        </h2>
+        <div className="text-[20px] md:text-[50px] text-white md:p-[24px]">
+          Learn
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
